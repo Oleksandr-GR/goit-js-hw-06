9b998cff67d0b8f5ad3b5c89af03b0ac05024790
Oleksandr Gryshko <alexandrgrishko@gmail.com>
2022-07-24 11:08:13 +0300

    HW6 final

--- js/task-08.js
+++ js/task-08.js
@@ -3,13 +3,14 @@ const logForm = document.querySelector('.login-form')
 logForm.addEventListener('submit', formSubmit)
 
 function formSubmit(event) {
+
     event.preventDefault()
-    
+
     const { elements: { email, password } } = event.currentTarget;
     if(email.value === ''|| password.value === '') {return alert("Пожалуйста, заполните все поля формы регистации")}
 
     const logData = {
-        email: email.value,
+        email: email.value, 
         password: password.value
     }
 
